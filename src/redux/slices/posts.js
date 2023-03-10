c09e72421f9c6c0b7fcffc8a16c84ee2419f9b0d
Ilyas Gibadullin <ilyas.gibadullin.199819987@gmail.com>
2023-03-10 07:15:30 +0300

    feat: 🎸 Fetch posts

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,4 +1,11 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import axios from "../../axios";
+
+export const fetchPost = createAsyncThunk("posts/fetchPosts", async () => {
+  const { data } = await axios("/posts");
+  return data;
+});
 
 const initialState = {
   posts: {
@@ -15,6 +22,20 @@ const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {},
+  extraReducers: {
+    [fetchPost.pending]: (state) => {
+      state.posts.items = [];
+      state.posts.status = "loading";
+    },
+    [fetchPost.fulfilled]: (state, action) => {
+      state.posts.status = "loaded";
+      state.posts.items = action.payload;
+    },
+    [fetchPost.rejected]: (state) => {
+      state.posts.status = "error";
+      state.posts.items = [];
+    },
+  },
 });
 
 export const postsReduces = postsSlice.reducer;
