71b5e7971bb320511a4581823175b5938cd62051
Ilyas Gibadullin <ilyas.gibadullin.199819987@gmail.com>
2023-03-13 18:06:29 +0300

    feat: 🎸 Create Auth Fn

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authReduces } from "./slices/auth";
 import { postsReduces } from "./slices/posts";
 
 const store = configureStore({
   reducer: {
     posts: postsReduces,
+    auth: authReduces,
   },
 });
 
