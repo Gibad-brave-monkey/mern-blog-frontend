2fdff003c122b1669e2b1a89dd74f0299bda2865
Ilyas Gibadullin <ilyas.gibadullin.199819987@gmail.com>
2023-03-14 17:37:46 +0300

    feat: 🎸 Add Auth

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -15,6 +15,15 @@ export const fetchAuthMe = createAsyncThunk("auth/fetchAuthMe", async () => {
   return data;
 });
 
+export const fetchRegister = createAsyncThunk(
+  "auth/fetchRegister",
+  async (params) => {
+    const { data } = await axios.post("/auth/register", params);
+
+    return data;
+  }
+);
+
 const initialState = {
   data: null,
   status: "loading",
@@ -50,6 +59,18 @@ const authSlice = createSlice({
       state.data = action.payload;
     },
     [fetchAuthMe.rejected]: (state) => {
+      state.status = "error";
+      state.data = null;
+    },
+    [fetchRegister.pending]: (state) => {
+      state.status = "loading";
+      state.data = null;
+    },
+    [fetchRegister.fulfilled]: (state, action) => {
+      state.status = "loaded";
+      state.data = action.payload;
+    },
+    [fetchRegister.rejected]: (state) => {
       state.status = "error";
       state.data = null;
     },
